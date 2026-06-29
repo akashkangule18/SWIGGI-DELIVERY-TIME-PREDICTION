235251fcc682617ee70efc349cc4b3ea218fb54d
Akash Kangule <akashkangule18@gmail.com>
2026-06-29 20:48:32 +0530

    fixed dagshub problem

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 
+import dagshub
 import mlflow
 import pandas as pd
 from fastapi import FastAPI
 from schema import DeliveryInput
 
+# ----------------------------------------------------
+# DagsHub Initialization
+# ----------------------------------------------------
 
-# MLflow Authentication
 username = os.getenv("DAGSHUB_USERNAME")
 token = os.getenv("DAGSHUB_TOKEN")
 
@@ -15,42 +18,51 @@ if not username or not token:
         "DAGSHUB_USERNAME or DAGSHUB_TOKEN environment variable is missing."
     )
 
+# Set MLflow credentials
 os.environ["MLFLOW_TRACKING_USERNAME"] = username
 os.environ["MLFLOW_TRACKING_PASSWORD"] = token
 
-# MLflow Tracking Server
-mlflow.set_tracking_uri(
-    "https://dagshub.com/akashkangule18/SWIGGI-DELIVERY-TIME-PREDICTION.mlflow"
+# Initialize DagsHub
+dagshub.init(
+    repo_owner="akashkangule18",
+    repo_name="SWIGGI-DELIVERY-TIME-PREDICTION",
+    mlflow=True
 )
 
-
+# ----------------------------------------------------
 # Load Registered Model
+# ----------------------------------------------------
+
 model = mlflow.sklearn.load_model(
     "models:/LightGBMRegressor@staging"
 )
 
+# ----------------------------------------------------
+# FastAPI App
+# ----------------------------------------------------
 
-# FastAPI
 app = FastAPI(
     title="Swiggy Delivery Time Prediction API",
     version="1.0",
     description="Predict Swiggy Delivery Time using a model stored in DagsHub Model Registry."
 )
 
-
+# ----------------------------------------------------
 # Home Endpoint
+# ----------------------------------------------------
+
 @app.get("/")
 def home():
     return {
         "message": "Swiggy Delivery Time Prediction API is Running!"
     }
 
-
-
+# ----------------------------------------------------
 # Prediction Endpoint
+# ----------------------------------------------------
+
 @app.post("/predict")
 def predict(data: DeliveryInput):
-
     try:
         input_df = pd.DataFrame([data.model_dump()])
 
